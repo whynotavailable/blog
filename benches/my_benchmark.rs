86292b80967598981ac4a50a603c81b12c6aad38
Tom <JosephDotson@belkonar.com>
2024-08-02 09:32:52 -0400

    benchmark

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,12 +1,18 @@
 use blog::match_route;
 use criterion::{criterion_group, criterion_main, Criterion};
 
-fn test_one() {
-    match_route("/post/david", "/post/:slug");
+fn test_one(route: &str, format: &str) {
+    match_route(route, format);
 }
 
 fn parsing_benchmark(c: &mut Criterion) {
-    c.bench_function("basic post route test", |b| b.iter(test_one));
+    c.bench_function("basic post route test", |b| {
+        b.iter(|| test_one("/post/david", "/post/:slug"))
+    });
+
+    c.bench_function("basic home route test", |b| {
+        b.iter(|| test_one("/home", "/home"))
+    });
 }
 
 criterion_group!(benches, parsing_benchmark);
